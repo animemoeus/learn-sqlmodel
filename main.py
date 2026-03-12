1dc6437b8324a5f4e979806b1f376cefeb89fb5d
Arter Tendean <arter.tendean.07@gmail.com>
2026-03-12 20:05:55 +0800

    Add initial SQLModel setup and Hero model definition

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-def main():
-    print("Hello from learn-sqlmodel!")
+from sqlmodel import Field, SQLModel, create_engine
 
+class Hero(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    name: str
+    secret_name: str
+    age: int | None = None
 
-if __name__ == "__main__":
-    main()
+sqlite_file_name = "database.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+
+engine = create_engine(sqlite_url, echo=True)
+
+SQLModel.metadata.create_all(engine)
+
+print("="*100)
