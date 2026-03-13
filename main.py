43b1bb7d86924a6e7cb4c373de7533b6c4d07eb3
Arter Tendean <arter.tendean.07@gmail.com>
2026-03-13 13:54:08 +0800

    Refactor main.py to include Session import and organize database setup and hero creation functions

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-from sqlmodel import Field, SQLModel, create_engine
+from sqlmodel import Field, SQLModel, create_engine, Session
 
 class Hero(SQLModel, table=True):
-    id: int | None = Field(default=None, primary_key=True)
-    name: str
-    secret_name: str
-    age: int | None = None
-
-class Hero2(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     name: str
     secret_name: str
@@ -17,6 +11,31 @@ sqlite_url = f"sqlite:///{sqlite_file_name}"
 
 engine = create_engine(sqlite_url, echo=True)
 
-SQLModel.metadata.create_all(engine)
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+
+def create_heroes():
+    hero_1 = Hero(name="Joko Widodo", secret_name="Mulyono")
+    hero_2 = Hero(name="Prabowo Subianto", secret_name="Kelapa Sawit")
+    hero_3 = Hero(name="Gibran", secret_name="Fufufafa", age=69)
+
+    session = Session(engine)
+
+    session.add(hero_1)
+    session.add(hero_2)
+    session.add(hero_3)
+
+    session.commit()
+    session.close()
+
+def main():
+    create_db_and_tables()
+    create_heroes()
+
+
+
+if __name__ == "__main__":
+    main()
 
 print("="*100)
