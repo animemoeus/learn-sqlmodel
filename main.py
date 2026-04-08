5975563430b314c39d1b570b024dc87207f2ac8d
Arter Tendean <arter.tendean.07@gmail.com>
2026-04-08 13:34:18 +0800

    Refactor create_heroes function to enhance session management and add detailed print statements for debugging

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 from sqlmodel import Field, SQLModel, create_engine, Session
 
+
 class Hero(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     name: str
     secret_name: str
     age: int | None = None
 
+
 sqlite_file_name = "database.db"
 sqlite_url = f"sqlite:///{sqlite_file_name}"
 
-engine = create_engine(sqlite_url, echo=True)
+engine = create_engine(
+    sqlite_url,
+     echo=True
+)
+
 
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
@@ -20,20 +26,58 @@ def create_heroes():
     hero_2 = Hero(name="Prabowo Subianto", secret_name="Kelapa Sawit")
     hero_3 = Hero(name="Gibran", secret_name="Fufufafa", age=69)
 
+    print("Before interacting with the database:")
+    print("Hero 1:", hero_1)
+    print("Hero 2:", hero_2)
+    print("Hero 3:", hero_3)
+
     with Session(engine) as session:
         session.add(hero_1)
         session.add(hero_2)
         session.add(hero_3)
 
+        print("After adding heroes to the session but before committing:")
+        print("Hero 1:", hero_1)
+        print("Hero 2:", hero_2)
+        print("Hero 3:", hero_3)
+
         session.commit()
+
+        print("After committing the session:")
+        print("Hero 1:", hero_1)
+        print("Hero 2:", hero_2)
+        print("Hero 3:", hero_3)
+
+        print("After committing the session, show ID values:")
+        print("Hero 1 ID:", hero_1.id)
+        print("Hero 2 ID:", hero_2.id)
+        print("Hero 3 ID:", hero_3.id)
+
+        print("Show the hero names after committing:")
+        print("Hero 1 Name:", hero_1.name)
+        print("Hero 2 Name:", hero_2.name)
+        print("Hero 3 Name:", hero_3.name)
+
+        session.refresh(hero_1)
+        session.refresh(hero_2)
+        session.refresh(hero_3) 
+        print("After refreshing the heroes")
+        print("Hero 1:", hero_1)
+        print("Hero 2:", hero_2)
+        print("Hero 3:", hero_3)
+
+    print("After closing the session:")
+    print("Hero 1:", hero_1)
+    print("Hero 2:", hero_2)
+    print("Hero 3:", hero_3)
+
 
 def main():
     create_db_and_tables()
     create_heroes()
 
 
-
 if __name__ == "__main__":
     main()
 
-print("="*100)
+print("=" * 100)
